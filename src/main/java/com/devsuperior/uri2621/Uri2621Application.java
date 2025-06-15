7562500ff7e14c9cb4223716fe6d12c86dd2183c
inacioads23 <jinacio11@gmail.com>
2025-06-15 14:07:11 -0300

    Updated Uri2621Application.java

--- src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,17 +1,26 @@
 package com.devsuperior.uri2621;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2621.dto.ProductsMinDto;
+import com.devsuperior.uri2621.projection.ProductMinProjection;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
 
 	@Autowired
-	private ProductRepository repository;
+	private final ProductRepository repository;
+	
+	public Uri2621Application(ProductRepository repository) {
+		this.repository = repository;
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2621Application.class, args);
@@ -19,6 +28,13 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<ProductMinProjection> list = repository.search1(10, 20, "p");
+		// converter para DTO
+		List<ProductsMinDto> result1 = list.stream().map(x -> new ProductsMinDto(x)).collect(Collectors.toList());
 		
+		System.out.println("\n*** RESULTADO SQL RAIZ: ***");
+		for(ProductsMinDto obj : result1) {
+			System.out.println(obj);
+		}
 	}
 }
