dde4a4e35a508950a8a7d88abd481f3b16fca7cf
inacioads23 <jinacio11@gmail.com>
2025-06-15 14:05:45 -0300

    Updated ProductsMinDto.java and ProductMinProjection.java

--- src/main/java/com/devsuperior/uri2621/dto/ProductsMinDto.java
+++ src/main/java/com/devsuperior/uri2621/dto/ProductsMinDto.java
@@ -4,17 +4,41 @@ import com.devsuperior.uri2621.projection.ProductMinProjection;
 
 public class ProductsMinDto {
 	
+	private Integer min;
+	private Integer max;
 	private String name;
 
 	public ProductsMinDto() {
 	}
 
-	public ProductsMinDto(String name) {
+	public ProductsMinDto(Integer min, Integer max, String name) {
+		super();
+		this.min = min;
+		this.max = max;
 		this.name = name;
 	}
 	
 	public ProductsMinDto(ProductMinProjection projection) {
+		min = projection.getMin();
+		max = projection.getMax();
 		name = projection.getName();
+	}
+
+	
+	public Integer getMin() {
+		return min;
+	}
+
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+
+	public Integer getMax() {
+		return max;
+	}
+
+	public void setMax(Integer max) {
+		this.max = max;
 	}
 
 	public String getName() {
@@ -24,7 +48,12 @@ public class ProductsMinDto {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	
-	
+	@Override
+	public String toString() {
+		return "ProductsMinDto [name=" + name + "]";
+	}
+
 
 }
--- src/main/java/com/devsuperior/uri2621/projection/ProductMinProjection.java
+++ src/main/java/com/devsuperior/uri2621/projection/ProductMinProjection.java
@@ -2,6 +2,8 @@ package com.devsuperior.uri2621.projection;
 
 public interface ProductMinProjection {
 	
+	Integer getMin();
+	Integer getMax();
 	String getName();
 
 }
