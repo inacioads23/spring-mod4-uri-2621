d2644e4620d52d15f02b0e837efeb0603da3d194
inacioads23 <jinacio11@gmail.com>
2025-06-15 14:06:26 -0300

    Updated ProductRepository.java

--- src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.uri2621.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projection.ProductMinProjection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	// SQL nativo
+	@Query(nativeQuery = true, value="SELECT pd.name "
+			+ "FROM products pd "
+			+ "INNER JOIN providers pr ON pd.id_providers = pr.id "
+			+ "WHERE pd.amount BETWEEN :min AND :max "
+			+ "AND pr.name LIKE CONCAT(UPPER(:beginName), '%')")
+	List<ProductMinProjection> search1(Integer min, Integer max, String beginName);
 
 }
